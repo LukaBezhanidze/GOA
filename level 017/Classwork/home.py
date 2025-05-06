e65d961e52f32273c74bffa8abb971892c657ce1
LukaBezhanidze <purplelotus220@gmail.com>
2025-05-07 00:57:08 +0400

    commit

--- level 017/Classwork/home.py
+++ level 017/Classwork/home.py
@@ -1,1 +1,7 @@
-#no cw
+
+def word_search(query, seq):
+    result = []
+    for item in seq:
+        if query.lower() in item.lower():
+            result.append(item)
+    return result if result else ["None"]
